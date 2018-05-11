8321f15ac65b5daef45710ee175f645614a7d9f7
HeJuKa <38949581+HeJuKa@users.noreply.github.com>
2018-05-11 17:22:04 +0200

    Add files via upload

---
+++ src/uhc/listener/ScenarioListener.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace uhc\listener;
+
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\EventPriority;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\plugin\MethodEventExecutor;
+use uhc\events\StartUHCEvent;
+use uhc\events\StopUHCEvent;
+use uhc\UHC;
+
+class ScenarioListener implements Listener
+{
+    private $plugin;
+
+    /**
+     * ScenarioListener constructor.
+     * @param UHC $plugin
+     */
+    public function __construct(UHC $plugin)
+    {
+        $this->setPlugin($plugin);
+        $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
+        $plugin->getServer()->getPluginManager()->registerEvent("uhc\\events\\StartUHCEvent", $this, EventPriority::NORMAL, new MethodEventExecutor("onStart"), $plugin, true);
+        $plugin->getServer()->getPluginManager()->registerEvent("uhc\\events\\StopUHCEvent", $this, EventPriority::NORMAL, new MethodEventExecutor("onStop"), $plugin, true);
+    }
+
+    /**
+     * @param PlayerMoveEvent $event
+     */
+    public function onMove(PlayerMoveEvent $event)
+    {
+        UHC::getScenariomanager()->doMove($event);
+    }
+
+    /**
+     * @param BlockBreakEvent $event
+     */
+    public function onBreak(BlockBreakEvent $event)
+    {
+        UHC::getScenariomanager()->doBreak($event);
+    }
+
+    /**
+     * @param BlockPlaceEvent $event
+     */
+    public function onPlace(BlockPlaceEvent $event)
+    {
+        UHC::getScenariomanager()->doPlace($event);
+    }
+
+    /**
+     * @param PlayerDeathEvent $event
+     */
+    public function onDeath(PlayerDeathEvent $event)
+    {
+        UHC::getScenariomanager()->doDeath($event);
+    }
+
+    /**
+     * @param EntityDamageEvent $event
+     */
+    public function onDamage(EntityDamageEvent $event)
+    {
+        UHC::getScenariomanager()->doDamage($event);
+    }
+
+    /**
+     * @param StartUHCEvent $event
+     */
+    public function onStart(StartUHCEvent $event)
+    {
+        UHC::getScenariomanager()->doStart($event);
+    }
+
+    /**
+     * @param StopUHCEvent $event
+     */
+    public function onStop(StopUHCEvent $event)
+    {
+        UHC::getScenariomanager()->doStop($event);
+    }
+
+    /**
+     * @return UHC
+     */
+    public function getPlugin() : UHC
+    {
+        return $this->plugin;
+    }
+
+    /**
+     * @param UHC $plugin
+     */
+    public function setPlugin(UHC $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+}
---
+++ src/uhc/listener/UHCListener.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace uhc\listener;
+
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\utils\TextFormat;
+use uhc\LangManager;
+use uhc\task\UHCTask;
+use uhc\UHC;
+use uhc\UHCPlayer;
+
+class UHCListener implements Listener
+{
+    private $plugin;
+
+    /**
+     * UHCListener constructor.
+     * @param UHC $plugin
+     */
+    public function __construct(UHC $plugin)
+    {
+        $this->setPlugin($plugin);
+        $plugin->getServer()->getPluginManager()->registerEvents($this, $plugin);
+    }
+
+    /**
+     * @param PlayerDeathEvent $event
+     */
+    public function onDeath(PlayerDeathEvent $event)
+    {
+        $player = $event->getPlayer();
+        if($player instanceof UHCPlayer){
+            if($player->isPlaying()){
+                UHC::getUHCManager()->setLastDeath($player->getName());
+                UHC::getUHCManager()->removePlayer($player);
+                $event->setDeathMessage(null);
+                UHC::getUHCManager()->sendMessage(LangManager::DEATH, true);
+                //TODO: Custom messages
+            }
+        }
+    }
+
+    /**
+     * @param EntityDamageEvent $event
+     */
+    public function onDamage(EntityDamageEvent $event)
+    {
+        if(!UHC::getUHCManager()->isPvP()) {
+            $event->setCancelled(true);
+        }
+    }
+
+    /**
+     * @param BlockBreakEvent $event
+     */
+    public function onBreak(BlockBreakEvent $event)
+    {
+        $player = $event->getPlayer();
+        if ($player instanceof UHCPlayer) {
+            if ($player->getLevel()->getName() === UHC::getUHCManager()->getName()) {
+                if (!UHC::getUHCManager()->isStarted() and UHC::getUHCManager()->getState() == UHCTask::STARTING) {
+                    $event->setCancelled(true);
+                }
+            }
+        }
+    }
+
+    /**
+     * @return UHC
+     */
+    public function getPlugin(): UHC
+    {
+        return $this->plugin;
+    }
+
+    /**
+     * @param UHC $plugin
+     */
+    public function setPlugin(UHC $plugin)
+    {
+        $this->plugin = $plugin;
+    }
+}
