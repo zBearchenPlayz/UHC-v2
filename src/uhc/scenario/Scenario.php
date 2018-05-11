05ccec4e5774a5f2b134dcca44af5001393bd8e2
HeJuKa <38949581+HeJuKa@users.noreply.github.com>
2018-05-11 17:24:45 +0200

    Create Scenario.php

---
+++ src/uhc/scenario/Scenario.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace uhc\scenario;
+
+
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use uhc\events\StartUHCEvent;
+use uhc\events\StopUHCEvent;
+
+abstract class Scenario
+{
+    private $enabled = false;
+    /**
+     * @param PlayerMoveEvent $event
+     */
+    public function onMove(PlayerMoveEvent $event) {}
+
+    /**
+     * @param BlockBreakEvent $event
+     */
+    public function onBreak(BlockBreakEvent $event) {}
+
+    /**
+     * @param BlockPlaceEvent $event
+     */
+    public function onPlace(BlockPlaceEvent $event) {}
+
+    /**
+     * @param PlayerDeathEvent $event
+     */
+    public function onDeath(PlayerDeathEvent $event) {}
+
+    /**
+     * @param StartUHCEvent $event
+     */
+    public function onStart(StartUHCEvent $event) {}
+
+    /**
+     * @param StopUHCEvent $event
+     */
+    public function onStop(StopUHCEvent $event) {}
+
+    /**
+     * @param EntityDamageEvent $event
+     */
+    public function onDamage(EntityDamageEvent $event) {}
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return "default";
+    }
+
+    /**
+     * @return bool
+     */
+    public function isEnabled() : bool
+    {
+        return $this->enabled;
+    }
+
+    /**
+     * @param bool $enabled
+     */
+    public function setEnabled(bool $enabled = true)
+    {
+        $this->enabled = $enabled;
+    }
+}
