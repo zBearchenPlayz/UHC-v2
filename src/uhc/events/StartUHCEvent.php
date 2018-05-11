f1091e6b2fa305c1c1fb1d180d1c5da198eeb039
HeJuKa <38949581+HeJuKa@users.noreply.github.com>
2018-05-11 17:18:05 +0200

    Create StartUHCEvent.php

---
+++ src/uhc/events/StartUHCEvent.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace uhc\events;
+
+
+use pocketmine\event\plugin\PluginEvent;
+use uhc\UHC;
+use uhc\UHCPlayer;
+
+class StartUHCEvent extends PluginEvent
+{
+    private $name = "";
+    private $players = [];
+
+    public static $handlerList = null;
+
+    /**
+     * StartUHCEvent constructor.
+     * @param UHC $plugin
+     * @param string $name
+     * @param UHCPlayer[] $players
+     */
+    public function __construct(UHC $plugin, string $name, array $players)
+    {
+        parent::__construct($plugin);
+        $this->setName($name);
+        $this->setPlayers($players);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return UHCPlayer[]
+     */
+    public function getPlayers() : array
+    {
+        return $this->players;
+    }
+
+    /**
+     * @param UHCPlayer[] $players
+     */
+    public function setPlayers(array $players)
+    {
+        $this->players = $players;
+    }
+}
